b1721c450c1c80023edccbdcbf68f119ff909fc5
nainshi880 <nainshijaiswal24@gmail.com>
2025-07-18 22:34:35 +0530

    remove unnecessary files

--- quiz-app/client/src/components/HighLightText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HighLightText = ({ children }) => {
-  return (
-    <span className='mx-3  bg-gradient-to-b from-green-400 px-4 h-max text-xl to-green-800 rounded-full '>{children}</span>
-  )
-}
-
-export default HighLightText
